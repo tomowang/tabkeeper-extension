0cf847540ca79999212fd6ac243afd4d6de7442a
Tomo Wang <tomo_wang@163.com>
2024-05-26 20:20:56 +0800

    feat: tab popover and info

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,38 @@
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  Stack,
+  Badge,
+} from '@chakra-ui/react'
+
 export default function Tab({tab}: {tab: chrome.tabs.Tab}) {
-  return <div title={tab.title} className={"border rounded flex items-center p-1" + (tab.active ? " border-blue-600" : "")}>
-    <img src={tab.favIconUrl} className={"w-5 h-5" + (tab.status === "unloaded" ? " rounded-full border-2 border-slate-500 border-dashed" : "")} />
-  </div>
+  // https://developer.chrome.com/docs/extensions/reference/api/tabs#type-TabStatus
+  let statusColor = "yellow";
+  if (tab.status === "complete") {
+    statusColor = "green";
+  } else if (tab.status === "unloaded") {
+    statusColor = "gray";
+  }
+  return <Popover trigger='hover'>
+    <PopoverTrigger>
+      <div className={"border rounded flex items-center p-1" + (tab.active ? " border-blue-600" : "")}>
+        <img src={tab.favIconUrl} className={"w-5 h-5" + (tab.status === "unloaded" ? " rounded-full border-2 border-slate-500 border-dashed" : "")} />
+      </div>
+    </PopoverTrigger>
+    <PopoverContent>
+      <PopoverHeader>{tab.title}</PopoverHeader>
+      <PopoverBody>
+        <p className='truncate'>{tab.url}</p>
+        <Stack direction='row'>
+          <Badge colorScheme={statusColor}>{tab.status}</Badge>
+          {tab.pinned && <Badge colorScheme='green'>Pinned</Badge>}
+          {tab.active && <Badge colorScheme='blue'>Active</Badge>}
+          {tab.mutedInfo?.muted && <Badge colorScheme='red'>Muted</Badge>}
+        </Stack>
+      </PopoverBody>
+    </PopoverContent>
+  </Popover>
 }
