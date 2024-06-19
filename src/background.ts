0547df034f99ab625b52c32ceb4eed32af2285fa
Tomo Wang <tomo_wang@163.com>
2024-06-19 21:53:46 +0800

    chore: format ts

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,26 @@
-const badgeColor = '#1B74E8'
+const badgeColor = "#1B74E8";
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('tabkeeper is installed');
-  void onTabsUpdated()
+  console.log("tabkeeper is installed");
+  void onTabsUpdated();
 });
 
 async function onTabsUpdated() {
-  const tabs = await chrome.tabs.query({})
-  const length = tabs.length
-  let text = '';
+  const tabs = await chrome.tabs.query({});
+  const length = tabs.length;
+  let text = "";
   if (length > 0) {
-    text = length.toString()
+    text = length.toString();
   }
-  await chrome.action.setBadgeText({ text })
-  await chrome.action.setBadgeBackgroundColor({ color: badgeColor })
+  await chrome.action.setBadgeText({ text });
+  await chrome.action.setBadgeBackgroundColor({ color: badgeColor });
 }
 
 chrome.tabs.onCreated.addListener(() => {
-  console.log('created')
-  void onTabsUpdated()
-})
+  console.log("created");
+  void onTabsUpdated();
+});
 chrome.tabs.onRemoved.addListener(() => {
-  console.log('removed')
-  void onTabsUpdated()
-})
+  console.log("removed");
+  void onTabsUpdated();
+});
